99142d998374bd0d3af72709343e33d45578cca2
Michael Mathias <mikeam996@gmail.com>
2023-02-18 07:42:55 -0800

    commit tree 709935227f6b82548d07d8990a9b80089c5edee0

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    console.log(cats.push(name));
+}
+
+function destructivelyPrependCat(name) {
+    console.log(cats.unshift(name));
+}
+
+function destructivelyRemoveLastCat() {
+    console.log(cats.pop());
+}
+
+function destructivelyRemoveFirstCat() {
+    console.log(cats.shift());
+}
+
+function appendCat(name) {
+    return [...cats, name];
+}
+
+console.log(appendCat("Broom"));
+
+function prependCat(name) {
+    return [name, ...cats];
+}
+
+console.log(prependCat("Arnold"));
+
+function removeLastCat() {
+    return cats.slice(0, 2);
+}
+
+console.log(removeLastCat());
+
+function removeFirstCat() {
+    return cats.slice(1);
+}
+
+console.log(removeFirstCat());
